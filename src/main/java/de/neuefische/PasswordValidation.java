16de3036567d9e709c93262ff20e8d44f10da138
Muslim Kareem <muslimkareem@icloud.com>
2023-05-09 17:06:40 +0200

    Big bang

---
+++ src/main/java/de/neuefische/PasswordValidation.java
@@ -0,0 +1,21 @@
+package de.neuefische;
+
+public class PasswordValidation {
+
+    public static boolean checkLength(String strToCheck) {
+        return  strToCheck.length() > 8;
+    }
+
+
+    public static boolean containsNumbers(String strToCheck) {
+        char[] c = strToCheck.toCharArray();
+        boolean tbr = false;
+        for (int i = 0; i < c.length; i++) {
+            if(Character.isDigit(c[i])){
+                tbr = true;
+                break;
+            }
+        }
+        return tbr;
+    }
+}
---
+++ src/test/java/de/neuefische/PasswordValidationTest.java
@@ -0,0 +1,48 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PasswordValidationTest {
+
+
+    @Test
+    public void checkLength_whenStringLongerThen8ReturnTrue(){
+        //GIVEN
+        String strToCheck = "this String much much longer then 8";
+
+        //WHEN
+        boolean actual = PasswordValidation.checkLength(strToCheck);
+
+        //THEN
+        Assertions.assertTrue(actual);
+    }
+
+    @Test
+    public void checkLength_whenStringSmallerThen8ReturnFalse(){
+        //GIVEN
+        String strToCheck = "";
+
+        //WHEN
+        boolean actual = PasswordValidation.checkLength(strToCheck);
+
+        //THEN
+        Assertions.assertFalse(actual);
+    }
+
+
+    @Test
+    public void containsNumbers_whenStringContainsNumbersReturnTrue(){
+        //GIVEN
+        String strToCheck = "lalalallalal 4";
+
+        //WHEN
+        boolean actual = PasswordValidation.containsNumbers(strToCheck);
+
+        //THEN
+        Assertions.assertTrue(actual);
+    }
+
+}
